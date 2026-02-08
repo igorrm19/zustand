434f75081e07de22a2cfb2883bdd9cb2bb08a3ff
igorrm23 <igormachadofire14@gmail.com>
2026-02-08 10:13:39 -0300

    ultimo commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useThemeStore } from './userThemerStory';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const isDarkMode = useThemeStore((state) => state.isDarkMode);
+  const toggleTheme = useThemeStore((state) => state.toggleTheme);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className={`container-header ${isDarkMode ? 'dark' : 'light'}`}>
+        <h1 className='title'>Mod Dark or Light</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className='container'>
+        <button className='button' onClick={toggleTheme}>{isDarkMode ? 'Light' : 'Dark'}</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
